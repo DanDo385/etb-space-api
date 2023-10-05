d79946dab7a2d2e9dc184e79b74364df52a86069
Daniel Magro <dando385@yahoo.com>
2023-10-05 13:50:58 -0400

    added images

--- src/App.js
+++ src/App.js
@@ -16,15 +16,20 @@ function App() {
   }, []); // empty dependency array ensures this useEffect runs once when the component mounts
 
   return (
-    <ul>
+    <ul className="flights-list">
       {flights.map((flight) => (
         <li key={flight.flight_number}>
+          <div className='flight-info'>
+            <img src={flight.links.mission_patch_small} alt={flight.mission_name} />
+          </div>
+          <div className='flight-data'>
           <h2>{flight.mission_name}</h2>
           <p>Flight Number: {flight.flight_number}</p>
           <p>Launched Date: {flight.launch_date_utc}</p>
           <p>Flight Details: {flight.details}</p>
           <p>Launch Year: {flight.launch_year}</p>
           <a href={flight.links.article_link}>Read More About the Launch...</a>
+          </div>
         </li>
       ))}
     </ul>
