343699c93e963d5bdeac413007bc9a3429436d3c
Daniel Magro <dando385@yahoo.com>
2023-10-05 13:24:24 -0400

    add axios logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [flights, setFlights] = useState([
-    {
-      flight_number: 1,
-      mission_name: "Mission 1",
-    },
-    {
-      flight_number: 2,
-      mission_name: "Mission 2",
-    },
-  ]);
-  
+  const [flights, setFlights] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.spacexdata.com/v2/launches')
+      .then((res) => {
+        setFlights(res.data);
+      })
+      .catch((err) => {
+        console.log("Error while fetching from the SpaceX API:", err);
+      });
+  }, []); // empty dependency array ensures this useEffect runs once when the component mounts
+
   return (
     <ul>
       {flights.map((flight) => (
         <li key={flight.flight_number}>
           <h2>{flight.mission_name}</h2>
-          <p>{flight.flight_number}</p>
+          <p>Flight Number: {flight.flight_number}</p>
         </li>
-      ))};
+      ))}
     </ul>
   );
 }
